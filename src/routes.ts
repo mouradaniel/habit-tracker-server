c2308b6d4b567d66b27662264f63418cfef8fb4a
Daniel Moura <daniel-moura@outlook.com>
2023-02-02 19:40:41 -0300

    feat: add endpoint day

--- src/routes.ts
+++ src/routes.ts
@@ -31,6 +31,50 @@ export async function appRoutes(app: FastifyInstance) {
     })
   })
 
+  app.get('/day', async (request) => {
+    const getDayParams = z.object({
+      date: z.coerce.date()
+    })
+
+    const { date } = getDayParams.parse(request.query)
+
+    const parsedDate = dayjs(date).startOf('day')
+    const weekDay = parsedDate.get('day')
+
+    console.log(parsedDate, weekDay)
+
+    const possibleHabits = await prisma.habit.findMany({
+      where: {
+        created_at: {
+          lte: date
+        },
+        weekDays: {
+          some: {
+            week_day: weekDay
+          }
+        }
+      }
+    })
+
+    const day = await prisma.day.findUnique({
+      where: {
+        date: parsedDate.toDate()
+      },
+      include: {
+        dayHabits: true
+      }
+    })
+
+    const completedHabits = day?.dayHabits.map(dayHabit => {
+      return dayHabit.habit_id
+    })
+
+    return {
+      possibleHabits,
+      completedHabits
+    }
+  })
+
   app.get('/habits', async () => {
     const habits = await prisma.habit.findMany()
 
