e824e20e2da516e4d698bee7b5a0ea7294322bcd
Daniel Moura <daniel-moura@outlook.com>
2023-02-01 21:55:28 -0300

    feat: create tables and relations

---
+++ prisma/migrations/20230202005426_create_relations/migration.sql
@@ -0,0 +1,55 @@
+/*
+  Warnings:
+
+  - You are about to drop the `day_habbits` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `habbits` table. If the table is not empty, all the data it contains will be lost.
+  - Added the required column `habitId` to the `habit_week_days` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- DropIndex
+DROP INDEX "day_habbits_day_id_habit_id_key";
+
+-- DropTable
+PRAGMA foreign_keys=off;
+DROP TABLE "day_habbits";
+PRAGMA foreign_keys=on;
+
+-- DropTable
+PRAGMA foreign_keys=off;
+DROP TABLE "habbits";
+PRAGMA foreign_keys=on;
+
+-- CreateTable
+CREATE TABLE "habits" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "title" TEXT NOT NULL,
+    "created_at" DATETIME NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "day_habits" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "day_id" TEXT NOT NULL,
+    "habit_id" TEXT NOT NULL,
+    CONSTRAINT "day_habits_day_id_fkey" FOREIGN KEY ("day_id") REFERENCES "days" ("id") ON DELETE RESTRICT ON UPDATE CASCADE,
+    CONSTRAINT "day_habits_habit_id_fkey" FOREIGN KEY ("habit_id") REFERENCES "habits" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
+-- RedefineTables
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_habit_week_days" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "habit_id" TEXT NOT NULL,
+    "week_day" INTEGER NOT NULL,
+    "habitId" TEXT NOT NULL,
+    CONSTRAINT "habit_week_days_habit_id_fkey" FOREIGN KEY ("habit_id") REFERENCES "habits" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+INSERT INTO "new_habit_week_days" ("habit_id", "id", "week_day") SELECT "habit_id", "id", "week_day" FROM "habit_week_days";
+DROP TABLE "habit_week_days";
+ALTER TABLE "new_habit_week_days" RENAME TO "habit_week_days";
+CREATE UNIQUE INDEX "habit_week_days_habit_id_week_day_key" ON "habit_week_days"("habit_id", "week_day");
+PRAGMA foreign_key_check;
+PRAGMA foreign_keys=ON;
+
+-- CreateIndex
+CREATE UNIQUE INDEX "day_habits_day_id_habit_id_key" ON "day_habits"("day_id", "habit_id");
