cf2d816018e93f279837e0801101d9f2ec072543
Daniel Moura <daniel-moura@outlook.com>
2023-02-03 13:46:47 -0300

    feat: toggle habit complete

--- src/routes.ts
+++ src/routes.ts
@@ -75,9 +75,51 @@ export async function appRoutes(app: FastifyInstance) {
     }
   })
 
-  app.get('/habits', async () => {
-    const habits = await prisma.habit.findMany()
+  app.patch('/habits/:id/toggle', async (request) => {
+    const toggleHabitParams = z.object({
+      id: z.string().uuid(),
+    })
 
-    return habits
+    const { id } = toggleHabitParams.parse(request.params)
+
+    const today = dayjs().startOf('day').toDate()
+
+    let day = await prisma.day.findUnique({
+      where: {
+        date: today
+      }
+    })
+
+    if (!day) {
+      day = await prisma.day.create({
+        data: {
+          date: today
+        }
+      })
+    }
+
+    const dayHabit = await prisma.dayHabit.findUnique({
+      where: {
+        day_id_habit_id: {
+          day_id: day.id,
+          habit_id: id
+        }
+      }
+    })
+
+    if (dayHabit) {
+      await prisma.dayHabit.delete({
+        where: {
+          id: dayHabit.id
+        }
+      })
+    } else {
+      await prisma.dayHabit.create({
+        data: {
+          day_id: day.id,
+          habit_id: id
+        }
+      })
+    }
   })
 }
