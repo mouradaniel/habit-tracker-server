2efa95965a755ce300ea8cc8c7e3bd85a7d3138b
Daniel Moura <daniel-moura@outlook.com>
2023-02-09 18:53:22 -0300

    fix: add cors and host to running local project on mobile

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,16 @@
 import Fastify from 'fastify';
+import cors from '@fastify/cors';
 
 import { appRoutes } from './routes';
 
 const app = Fastify();
 
+app.register(cors);
 app.register(appRoutes);
 
 app.listen({
-  port: 3000
+  port: 3000,
+  host: '0.0.0.0'
 }).then(() => {
   console.log('Server is running on port 3000.')
 })
